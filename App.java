f4d3fd09d2d0c93540b3717dc73ba5439866fd40
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-27 17:23:48 +0530

    Added Welcome file

---
+++ App.java
@@ -0,0 +1,13 @@
+package bridgelabz.Regex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
