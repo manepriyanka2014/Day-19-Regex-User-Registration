9950f7bddc7d3b78c008b7a30431062f3eec22e8
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-29 13:20:35 +0530

    Added user Email ID

--- UserDetails.java
+++ UserDetails.java
@@ -1,4 +1,5 @@
 package bridgelabz.Regex;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,6 +21,17 @@ public class UserDetails {
         regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(lastName);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("InValid");
+        }
+    }
+
+    public void emailId(String emailId) {
+        regex = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z]+.[a-z]{2,3}$";
+        Pattern pattern  = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(emailId);
         if (matcher.matches()) {
             System.out.println("Valid");
         } else {
